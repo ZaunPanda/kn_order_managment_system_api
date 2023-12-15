9ee17f0c2772647749f02d2e43d26a83b1874d5d
Juri Alb <strifers@gmail.com>
2023-12-15 18:43:44 +0200

    Docker itegration

--- src/main/resources/db/changelog/00001_init_db.sql
+++ src/main/resources/db/changelog/00001_init_db.sql
@@ -1,32 +1,55 @@
 CREATE TABLE IF NOT EXISTS customers
 (
     `registration_code` INT PRIMARY KEY AUTO_INCREMENT,
-    `fullName`         VARCHAR(255),
+    `full_name`         VARCHAR(255),
     `email`            VARCHAR(255),
     `telephone`        VARCHAR(20)
 );
+
+INSERT INTO customers (full_name, email, telephone) VALUES
+                                                       ('John Doe', 'johndoe@example.com', '123-456-7890'),
+                                                       ('Jane Smith', 'janesmith@example.com', '987-654-3210'),
+                                                       ('Peter Jones', 'peterjones@example.com', '098-765-4321');
+
 CREATE TABLE IF NOT EXISTS orders
 (
-    `orderId`        INT PRIMARY KEY AUTO_INCREMENT,
+    `order_id`        INT PRIMARY KEY AUTO_INCREMENT,
     `customer_id`     INT,
-    `submissionDate` DATE,
+    `submission_date` DATE,
     FOREIGN KEY (`customer_id`) REFERENCES `customers` (`registration_code`)
 );
 
+INSERT INTO orders (customer_id, submission_date) VALUES
+                                                     (1, '2023-10-04'),
+                                                     (2, '2023-10-05'),
+                                                     (3, '2023-10-06');
+
 CREATE TABLE IF NOT EXISTS products
 (
-    `productId`   INT PRIMARY KEY AUTO_INCREMENT,
-    `productName` VARCHAR(255),
-    `skuCode` varchar(255),
-    `unitPrice` INT
+    `product_id`   INT PRIMARY KEY AUTO_INCREMENT,
+    `product_name` VARCHAR(255),
+    `sku_code` varchar(255),
+    `unit_price` INT
 );
+
+INSERT INTO products (product_name, sku_code, unit_price) VALUES
+                                                           ('Laptop', 'ABC123', 1000),
+                                                           ('Smartphone', 'DEF456', 500),
+                                                           ('Tablet', 'GHI789', 300);
+
 CREATE TABLE IF NOT EXISTS order_lines
 (
     `orderline_id` INT PRIMARY KEY AUTO_INCREMENT,
-    `orderId`     INT,
-    `productId`   INT,
+    `order_id`     INT,
+    `product_id`   INT,
     `quantity`     INT,
-    FOREIGN KEY (`orderId`) REFERENCES orders (`orderId`),
-    FOREIGN KEY (`productId`) REFERENCES products (`productId`)
+    FOREIGN KEY (`order_id`) REFERENCES `orders` (`order_id`),
+    FOREIGN KEY (`product_id`) REFERENCES `products` (`product_id`)
 );
 
+INSERT INTO order_lines (order_id, product_id, quantity) VALUES
+                                                           (1, 1, 1),
+                                                           (1, 2, 2),
+                                                           (2, 3, 3),
+                                                           (3, 1, 1),
+                                                           (3, 2, 2);
